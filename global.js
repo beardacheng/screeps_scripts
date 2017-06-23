c0ff00f04deb1ce2fa20ce8bffcd43f6d9d1f051
beardacheng <will@vip.qq.com>
2017-06-23 19:14:59 +0800

    开始进程间通讯问题2

--- global.js
+++ global.js
@@ -21,38 +21,32 @@ var Global = _.assign(_.clone(Tool.Singleton), {
 		
 		ins.SendData = function(data) {
 			_.each(ins._otherIDs, function(v) {
-				console.log(v);
-				console.log("!!" + RawMemory.segments[v]);
 				var org = JSON.parse(RawMemory.segments[v]);
 				org.push(data);
 				RawMemory.segments[v] = JSON.stringify(org);
-				RawMemory.setActiveSegments([v]);
 			})
 		}
 		
 		ins.RecvData = function() { 
-			var memory = RawMemory.segments[ins._id];
-			console.log("recvdata : " + memory);
-			
-			/*
+			var memory = JSON.parse(RawMemory.segments[ins._id]);			
 			if (_.size(memory) > 0) {
-				var data = _.head(memory);
+				var data = _.head(memory); 
 				RawMemory.segments[ins._id] = JSON.stringify(_.drop(memory, 1));
+				return data;
 			}
-			*/
+			
+			return null;
 		}
 		
 		ins.tick = function() {
+			RawMemory.setActiveSegments([0,1]);
 			if (preticked) {
 				preticked = true;
-				return;
+				return; 
 			}
 			
-			console.log('i am ' + ins._id);
 			ins.SendData(Game.time);
-			console.log('send ' + Game.time);
-			console.log('recv ' + ins.RecvData());
-			console.log('i am ' + ins._id + ' end');
+			var data = ins.RecvData();
 		}
 		
 		return ins;
